5ea5c6abf60541b89e5995c5d90de0244a30ccaa
Ivan Kuchin <tadump+git@gmail.com>
2014-05-03 18:08:24 +0200

    cleanup spaces in viz.js

--- viz.js
+++ viz.js
@@ -83,7 +83,7 @@ function readFilesAndLoad(files, loader) {
         reportError('your browser does not have the necessary file reader API.');
         return;
     }
-    
+
     var left = files.length
     var texts = []
 
@@ -123,14 +123,14 @@ function handleDrop(e) {
     [].forEach.call(cols, function (col) {
         col.classList.remove('over');
     });
-    
+
     // See the section on the DataTransfer object.
     files = getFiles(e)
     if(!files){
         return false;
     }
 
-    //document.getElementById('debug').innerHTML = file; 
+    //document.getElementById('debug').innerHTML = file;
     readFilesAndLoad(files, loadFiles);
     return false;
 }
@@ -140,23 +140,20 @@ function handleDrop(e) {
  ***/
 function setup() {
 
-	var cols = document.querySelectorAll('.drop_zone');
-	[].forEach.call(cols, function(col) {
-		col.addEventListener('dragstart', handleDragStart, false);
-		col.addEventListener('dragenter', handleDragEnter, false);
-		col.addEventListener('dragleave', handleDragLeave, false);
-		col.addEventListener('dragover',  handleDragOver, false);
-		col.addEventListener('dragend',   handleDragEnd, false);
-	});
+    var cols = document.querySelectorAll('.drop_zone');
+    [].forEach.call(cols, function(col) {
+        col.addEventListener('dragstart', handleDragStart, false);
+        col.addEventListener('dragenter', handleDragEnter, false);
+        col.addEventListener('dragleave', handleDragLeave, false);
+        col.addEventListener('dragover',  handleDragOver, false);
+        col.addEventListener('dragend',   handleDragEnd, false);
+    });
 
-	var cols = document.querySelectorAll('#drop_zone');
-	[].forEach.call(cols, function(col) {
-		col.addEventListener('drop', handleDrop, false);
-	});
+    var cols = document.querySelectorAll('#drop_zone');
+    [].forEach.call(cols, function(col) {
+        col.addEventListener('drop', handleDrop, false);
+    });
 
 }
 
 setup();
-
-
-
